b401be3550805fbb73ee0d41e73797831e7abae5
clutchx <charles.caron1992@gmail.com>
2018-02-01 16:00:29 +0100

    Articles list

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,4 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(){
+        return {
+            articles: [
+                {
+                    Id: 1,
+                    Name: "Article 1",
+                    Description: "Description article 1"
+                },
+                {
+                    Id: 2,
+                    Name: "Article 2",
+                    Description: "Description article 2"
+                },
+                {
+                    Id: 3,
+                    Name: "Article 3",
+                    Description: "Description article 3"
+                }
+            ]
+        }
+    }
 });
